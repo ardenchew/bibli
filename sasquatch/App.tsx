80e2c4f986b6fc8715982c6067badbf592fe9e95
Arden Chew <achew@grailbio.com>
2023-09-14 01:01:06 -0700

    very rudimentary Auth0 integration working

--- sasquatch/App.tsx
+++ sasquatch/App.tsx
@@ -1,118 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Button, Text, View} from 'react-native';
+import {useAuth0, Auth0Provider} from 'react-native-auth0';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const LoginButton = () => {
+  const {authorize} = useAuth0();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const onPress = async () => {
+    try {
+      console.log('LoginButton hello what the fuck');
+      await authorize();
+    } catch (e) {
+      console.log('LoginButton hello what the fuck');
+      console.log(e);
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  return <Button onPress={onPress} title="Log in" />;
+};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const LogoutButton = () => {
+  const {clearSession} = useAuth0();
+
+  const onPress = async () => {
+    console.log('LogoutButton hello what the fuck');
+    try {
+      await clearSession();
+    } catch (e) {
+      console.log('LogoutButton hello what the fuck');
+      console.log(e);
+    }
+  };
+
+  return <Button onPress={onPress} title="Log out" />;
+};
+
+const Profile = () => {
+  const {user} = useAuth0();
+
+  return (
+    <>
+      {user && <Text>Logged in as {user.name}</Text>}
+      {!user && <Text>Not logged in</Text>}
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Auth0Provider
+      domain={'dev-xlrahc2qy1wqddf8.us.auth0.com'}
+      clientId={'bM3WrVIqXJmrBkEty0ofS8RyBvrM14Pl'}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+      </View>
+    </Auth0Provider>
+  );
+};
 
 export default App;
