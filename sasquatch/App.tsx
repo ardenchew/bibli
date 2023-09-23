50d1ae24ce48f8660dfbb485fd929a9714a0d408
Arden Chew <arden@thechewfamily.com>
2023-09-22 22:09:06 -0700

    updated yarn.lock, added react native paper, added react navigator router, updated auth0 settings (#14)
    * updated yarn.lock, added react native paper, moved sign in to screen
    
    * working splash screen
    
    * working navigation and login prompt
    
    * working auth0 naming with no prompt log out
    
    * image restoration
    
    ---------
    
    Co-authored-by: Arden Chew <achew@grailbio.com>

--- sasquatch/App.tsx
+++ sasquatch/App.tsx
@@ -1,65 +1,16 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
-import {useAuth0, Auth0Provider} from 'react-native-auth0';
-
-const LoginButton = () => {
-  const {authorize} = useAuth0();
-
-  const onPress = async () => {
-    try {
-      console.log('LoginButton hello what the fuck');
-      await authorize();
-    } catch (e) {
-      console.log('LoginButton hello what the fuck');
-      console.log(e);
-    }
-  };
-
-  return <Button onPress={onPress} title="Log in" />;
-};
-
-const LogoutButton = () => {
-  const {clearSession} = useAuth0();
-
-  const onPress = async () => {
-    console.log('LogoutButton hello what the fuck');
-    try {
-      await clearSession();
-    } catch (e) {
-      console.log('LogoutButton hello what the fuck');
-      console.log(e);
-    }
-  };
-
-  return <Button onPress={onPress} title="Log out" />;
-};
-
-const Profile = () => {
-  const {user} = useAuth0();
-
-  return (
-    <>
-      {user && <Text>Logged in as {user.name}</Text>}
-      {!user && <Text>Not logged in</Text>}
-    </>
-  );
-};
+import {PaperProvider} from 'react-native-paper';
+import {Auth0Provider} from 'react-native-auth0';
+import Router from './src';
 
 const App = () => {
   return (
     <Auth0Provider
       domain={'dev-xlrahc2qy1wqddf8.us.auth0.com'}
       clientId={'bM3WrVIqXJmrBkEty0ofS8RyBvrM14Pl'}>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <LoginButton />
-        <LogoutButton />
-        <Profile />
-      </View>
+      <PaperProvider>
+        <Router />
+      </PaperProvider>
     </Auth0Provider>
   );
 };
---
+++ sasquatch/src/screens/HomeScreen.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+import {PaperProvider} from 'react-native-paper';
+import {useAuth0, Auth0Provider} from 'react-native-auth0';
+
+const LoginButton = () => {
+  const {authorize} = useAuth0();
+
+  const onPress = async () => {
+    try {
+      await authorize({additionalParameters: {prompt: 'login'}});
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return <Button onPress={onPress} title="Log in" />;
+};
+
+const LogoutButton = () => {
+  const {clearCredentials} = useAuth0();
+
+  const onPress = async () => {
+    console.log('LogoutButton hello what the fuck');
+    try {
+      await clearCredentials();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return <Button onPress={onPress} title="Log out" />;
+};
+
+const Profile = () => {
+  const {user} = useAuth0();
+
+  return (
+    <>
+      {user && <Text>Logged in as {user.name}</Text>}
+      {!user && <Text>Not logged in</Text>}
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Auth0Provider
+      domain={'dev-xlrahc2qy1wqddf8.us.auth0.com'}
+      clientId={'bM3WrVIqXJmrBkEty0ofS8RyBvrM14Pl'}>
+      <PaperProvider>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </View>
+      </PaperProvider>
+    </Auth0Provider>
+  );
+};
+
+export default App;
